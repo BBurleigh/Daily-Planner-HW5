a23d70dcac067d16d7fbdd9baffd8651b7179e1e
Brayden Burleigh <brayburl224@gmail.com>
2022-09-19 20:34:59 -0400

    added classes to time frames as well as names to text boxes --> also removed CSS from selectable boxes that were not being used (excess)

--- Develop/JS/script.js
+++ Develop/JS/script.js
@@ -1,15 +1,3 @@
 var today = moment();
 
 $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
-
-$( function() {
-
-  $("#selectable").selectable();
-
-});
-
-//function createTable {
-
- // var 
-
-//}
